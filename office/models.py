7286b438d4ef447fb986a4c9dc9f59a9fa853ff9
hell0worId <arzookharayat@gmail.com>
2023-12-04 16:23:50 +0530

    first_commit

---
+++ office/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+# Create your models here.
+class Patient(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(120)])
+    heartrate = models.IntegerField(default=60,validators=[MinValueValidator(1),MaxValueValidator(300)])
+    
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name} is {self.age} years old."
+    
